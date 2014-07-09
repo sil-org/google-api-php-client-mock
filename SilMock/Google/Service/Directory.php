922a2f64b2efb231775e5e826c8c694ee1a7d03b
Steve Bagwell <baggerone@gmail.com>
2014-07-09 14:05:14 -0400

    added use statements and fixed statments that instantiate new classes

--- SilMock/Google/Service/Directory.php
+++ SilMock/Google/Service/Directory.php
@@ -1,6 +1,9 @@
 <?php
 namespace SilMock\Google\Service;
 
+use SilMock\Google\Service\Directory\Users_Resource;
+use SilMock\Google\Service\Directory\UsersAliases_Resource;
+
 class Directory {
 
     public $users;
@@ -12,8 +15,8 @@ class Directory {
      */
     public function __construct($client)
     {
-        $this->users = new Directory\Users_Resource();
-        $this->users_aliases = new Directory\UsersAliases_Resource();
+        $this->users = new Users_Resource();
+        $this->users_aliases = new UsersAliases_Resource();
     }
 
 } 
