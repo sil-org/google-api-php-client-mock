4e55076a8ba35f2887b2409be7d6b77439ff4ad7
Steve Bagwell <baggerone@gmail.com>
2014-07-11 08:36:47 -0400

    added delete test added test for Exception when user doesn't exist in db

--- SilMock/tests/Google/Service/DirectoryTest.php
+++ SilMock/tests/Google/Service/DirectoryTest.php
@@ -194,6 +194,38 @@ class DirectoryTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $results, $msg);
     }
 
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionCode 201407101130
+     */
+    public function testUsersUpdate_NotThere()
+    {
+        $fixturesClass = new GoogleFixtures();
+        $fixturesClass->removeAllFixtures();
+
+        $userId = 999999;
+
+        $userData = array(
+            "changePasswordAtNextLogin" => false,
+            "hashFunction" => "SHA-1",
+            "id" => $userId,
+            "password" => "testP4ss",
+            "primaryEmail" => "user_test4@sil.org",
+            "suspended" => false,
+            "suspensionReason" => null
+        );
+
+        $fixtures = $this->get_fixtures();
+        $fixturesClass->addFixtures($fixtures);
+
+        $newUser = new User();
+        $newUser->initialize($userData);
+
+        $newDir = new Directory(null);
+        $newDir->users->update($userId, $newUser);
+        // the assert is in the doc comment
+    }
+
     public function testUsersDelete()
     {
         $fixturesClass = new GoogleFixtures();
@@ -293,6 +325,27 @@ class DirectoryTest extends PHPUnit_Framework_TestCase
 
         $msg = " *** Bad data from sqlite database";
         $this->assertEquals($expected, $results, $msg);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionCode 201407110830
+     */
+    public function testUsersAliasesInsert_UserNotThere()
+    {
+        $fixturesClass = new GoogleFixtures();
+        $fixturesClass->removeAllFixtures();
+
+        $fixtures = $this->get_fixtures();
+        $fixturesClass->addFixtures($fixtures);
+
+        $newAlias = new Alias();
+        $newAlias->alias = "users_alias1@sil.org";
+        $newAlias->kind = "personal";
+
+        $newDir = new Directory(null);
+        $newAlias = $newDir->users_aliases->insert("no_user@sil.org", $newAlias);
+        // the assert is in the doc comments with @expectedException
     }
 
     public function testUsersAliasesListUsersAliases_Email()
@@ -399,6 +452,67 @@ class DirectoryTest extends PHPUnit_Framework_TestCase
         $results = $user_aliases;
         $expected = array("users_alias2@sil.org", "users_alias7@sil.org");
         $msg = " *** Bad returned Aliases";
+        $this->assertEquals($expected, $results, $msg);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionCode 201407101420
+     */
+    public function testUsersAliasesListUsersAliases_UserNotThere()
+    {
+        $fixturesClass = new GoogleFixtures();
+        $fixturesClass->removeAllFixtures();
+
+        $fixtures = $this->get_fixtures();
+        $fixturesClass->addFixtures($fixtures);
+
+        $newFixtures = array(
+            array('directory', 'users_alias', '{"id":1,"primaryEmail":' .
+            '"user_test1@sil.org","alias":"users_alias7@sil.org"}'),
+        );
+        $fixturesClass->addFixtures($newFixtures);
+
+        $newDir = new Directory(null);
+        $aliases = $newDir->users_aliases->listUsersAliases("no_user@sil.org");
+    }
+
+    public function testUsersAliasesDelete()
+    {
+        $fixturesClass = new GoogleFixtures();
+        $fixturesClass->removeAllFixtures();
+
+        $fixtures = $this->get_fixtures();
+        $fixturesClass->addFixtures($fixtures);
+
+        $newFixtures = array(
+            array('directory', 'users_alias', '{"id":1,"primaryEmail":' .
+            '"user_test1@sil.org","alias":"users_alias7@sil.org"}'),
+        );
+        $fixturesClass->addFixtures($newFixtures);
+
+        $newDir = new Directory(null);
+        $results = $newDir->users_aliases->delete("user_test1@sil.org",
+                                                 "users_alias2@sil.org");
+
+        $this->assertTrue($results, " *** Didn't appear to delete the alias.");
+
+        $sqliteUtils = new SqliteUtils();
+        $results = $sqliteUtils->getData('directory', 'users_alias');
+
+        $expected = array(
+            array('id' => '6',
+                  'type' => 'directory',
+                  'class' => 'users_alias',
+                  'data' => '{"id":1,"alias":"users_alias6@sil.org"}',
+            ),
+            array('id' => '7',
+                'type' => 'directory',
+                'class' => 'users_alias',
+                'data' => '{"id":1,"primaryEmail":' .
+                      '"user_test1@sil.org","alias":"users_alias7@sil.org"}'),
+        );
+        $msg = " *** Mismatching users_aliases in db";
         $this->assertEquals($expected, $results, $msg);
 
     }
