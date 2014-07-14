eda9e673409d8da9dd714257df0d091c3f72cf6a
Steve Bagwell <baggerone@gmail.com>
2014-07-14 16:48:07 -0400

    commented out defining properties and added functions for object access

--- SilMock/Google/Service/Directory/User.php
+++ SilMock/Google/Service/Directory/User.php
@@ -7,21 +7,49 @@ class User implements \ArrayAccess
 
     protected $_values = array();
 
-    public $changePasswordAtNextLogin; // bool
-    public $hashFunction; // string
-    public $id; // int???
-    public $password; // string
-    public $primaryEmail; // string email
-    public $suspended; // bool
-    public $suspensionReason; // string
+//    public $changePasswordAtNextLogin; // bool
+//    public $hashFunction; // string
+//    public $id; // int???
+//    public $password; // string
+//    public $primaryEmail; // string email
+//    public $suspended; // bool
+//    public $suspensionReason; // string
+//    public $aliases = array();
 
     public function initialize($properties)
     {
+        $this->aliases = array();
         foreach ($properties as $key=>$value) {
             $this->$key = $value;
         }
     }
 
+    /**
+     * Get a data by property name
+     *
+     * @param string The key data to retrieve
+     */
+    public function &__get ($key) {
+        if ($key === 'aliases') {
+            if (isset($this->aliases)) {
+                $this->_values['aliases'] = $this->aliases;
+            } else {
+                $this->_values['aliases'] = array();
+            }
+        }
+        return $this->_values[$key];
+    }
+
+    /**
+     * Assigns a value to the specified property
+     *
+     * @param string The data key to assign the value to
+     * @param mixed  The value to set
+     */
+    public function __set($key,$value) {
+        $this->_values[$key] = $value;
+        $this->$key = $value;
+    }
 
     // These are for implementing the ArrayAccess
     public function offsetExists($offset) {
