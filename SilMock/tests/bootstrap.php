899e223343947d1416df81e4b6a5c27c02debf1f
Phillip Shipley <phillip_shipley@sil.org>
2014-07-29 14:21:57 -0400

    added travis badge, updated unit test bootstrap and xml

--- SilMock/tests/bootstrap.php
+++ SilMock/tests/bootstrap.php
@@ -4,11 +4,11 @@
 //AutoLoader::registerDirectory(__DIR__ . '/../../SilMock');
 
 
-function testAutoloader($className) {
-    $pathAndFile = __DIR__ . '/../../' . $className . '.php';
-    if (file_exists($pathAndFile)) {
-            require_once $pathAndFile;
-    }
-
-}
-spl_autoload_register('testAutoloader');
+//function testAutoloader($className) {
+//    $pathAndFile = __DIR__ . '/../../' . $className . '.php';
+//    if (file_exists($pathAndFile)) {
+//            require_once $pathAndFile;
+//    }
+//
+//}
+//spl_autoload_register('testAutoloader');
