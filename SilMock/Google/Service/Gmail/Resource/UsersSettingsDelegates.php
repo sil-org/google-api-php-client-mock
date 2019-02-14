c9b1be414f615a04aa2782ce99aed10bc2409052
Matt Henderson <matt_henderson@sil.org>
2019-02-14 16:51:15 -0500

    Refactor, expand mock methods for delegated access

--- SilMock/Google/Service/Gmail/Resource/UsersSettingsDelegates.php
+++ SilMock/Google/Service/Gmail/Resource/UsersSettingsDelegates.php
@@ -34,18 +34,94 @@ class UsersSettingsDelegates
      */
     public function create($userId, \Google_Service_Gmail_Delegate $postBody, $optParams = array())
     {
-        // If the userId is not an email address, it must be an id.
-        $key = 'primaryEmail';
+        $this->assertIsValidUserId($userId);
+        
+        if ($this->hasDelegate($userId, $postBody->delegateEmail)) {
+            throw new \Google_Service_Exception('Already has delegate', 409);
+        }
+        
+        return $this->addDelegate($userId, $postBody->delegateEmail);
+    }
+    
+    protected function hasDelegate(string $userId, string $delegateEmail): bool
+    {
+        foreach ($this->listDelegatesFor($userId) as $delegate) {
+            if ($delegate->delegateEmail === $delegateEmail) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    protected function listDelegatesFor(string $userId): array
+    {
+        $matchingRecords = [];
+        foreach ($this->getDelegateRecords() as $delegateRecord) {
+            $delegate = json_decode($delegateRecord['data']);
+            if ($delegate !== null && $delegate->primaryEmail === $userId) {
+                $matchingRecord = new \Google_Service_Gmail_Delegate();
+                $matchingRecord->setDelegateEmail($delegate->delegateEmail);
+                $matchingRecord->setVerificationStatus($delegate->verificationStatus);
+                $matchingRecords[$delegateRecord['id']] = $matchingRecord;
+            }
+        }
+        return $matchingRecords;
+    }
+    
+    /**
+     * Get the delegate records. Example result:
+     *
+     *     [
+     *         [
+     *             'id' => '1',
+     *             'type' => 'gmail',
+     *             'class' => 'users_settings_delegate',
+     *             'data' => '{"delegateEmail":"john_smith@example.org","verificationStatus":"accepted","primaryEmail":"other_person@example.org"}',
+     *         ],
+     *         // ...
+     *     ]
+     *
+     * @return array[]
+     */
+    protected function getDelegateRecords(): array
+    {
+        $sqliteUtils = $this->getSqliteUtils();
+        return $sqliteUtils->getData($this->dataType, $this->dataClass);
+    }
+    
+    protected function addDelegate(string $userId, string $delegateEmail)
+    {
+        $sqliteUtils = $this->getSqliteUtils();
+        $sqliteUtils->recordData(
+            $this->dataType,
+            $this->dataClass,
+            json_encode([
+                'primaryEmail' => $userId,
+                'delegateEmail' => $delegateEmail,
+                'verificationStatus' => 'accepted',
+            ])
+        );
+        
+        return $this->get($userId, $delegateEmail);
+    }
+    
+    protected function getSqliteUtils(): SqliteUtils
+    {
+        return new SqliteUtils($this->dbFile);
+    }
+    
+    protected function assertIsValidUserId(string $userId)
+    {
         if (! $this->isValidEmailAddress($userId)) {
-            $key = 'id';
-            $userId = intval($userId);
+            throw new \Google_Service_Exception('Invalid userId: ' . $userId, 400);
         }
-        
-        if ($postBody->$key === null) {
-            $postBody->$key = $userId;
+    }
+    
+    protected function assertIsValidDelegateEmail($delegateEmail)
+    {
+        if (! $this->isValidEmailAddress($delegateEmail)) {
+            throw new \Google_Service_Exception('Invalid delegate: ' . $delegateEmail, 400);
         }
-        
-        return $this->insertAssumingUserExists($postBody);
     }
     
     /**
@@ -59,33 +135,39 @@ class UsersSettingsDelegates
         return (filter_var($email, FILTER_VALIDATE_EMAIL) !== false);
     }
     
-    /**
-     * Add a delegate to a user, assuming the user is already in the database.
-     *
-     * @param Google_Service_Gmail_Delegate $postBody - The object with the data
-     *     for that delegate.
-     * @return Google_Service_Gmail_Delegate - A real
-     *     Google_Service_Gmail_Delegate instance.
-     */
-    public function insertAssumingUserExists($postBody)
+    public function get($userId, $delegateEmail, $optParams = array())
     {
-        $entryData = json_encode(get_object_vars($postBody));
-        $sqliteUtils = new SqliteUtils($this->dbFile);
-        $sqliteUtils->recordData(
-            $this->dataType,
-            $this->dataClass,
-            $entryData
-        );
-        $allDelegates = $sqliteUtils->getData($this->dataType, $this->dataClass);
+        $this->assertIsValidUserId($userId);
+        $this->assertIsValidDelegateEmail($delegateEmail);
         
-        if (! $allDelegates) {
-            return null;
+        foreach ($this->listDelegatesFor($userId) as $delegate) {
+            if ($delegate->delegateEmail === $delegateEmail) {
+                $matchingRecord = new \Google_Service_Gmail_Delegate();
+                $matchingRecord->setDelegateEmail($delegate->delegateEmail);
+                $matchingRecord->setVerificationStatus($delegate->verificationStatus);
+                return $matchingRecord;
+            }
         }
+        throw new \Google_Service_Exception('Invalid delegate', 404);
+    }
+    
+    public function delete($userId, $delegateEmail, $optParams = array())
+    {
+        $this->assertIsValidUserId($userId);
+        $this->assertIsValidDelegateEmail($delegateEmail);
         
-        $newDelegate = new \Google_Service_Gmail_Delegate();
-        ObjectUtils::initialize($newDelegate, $postBody);
-        $newDelegate->verificationStatus = 'accepted';
-        
-        return $newDelegate;
+        foreach ($this->listDelegatesFor($userId) as $recordId => $delegate) {
+            if ($delegate->delegateEmail === $delegateEmail) {
+                $this->removeDelegate($recordId);
+                return;
+            }
+        }
+        throw new \Google_Service_Exception('Invalid delegate', 404);
+    }
+    
+    protected function removeDelegate(int $recordId)
+    {
+        $sqliteUtils = $this->getSqliteUtils();
+        $sqliteUtils->deleteRecordById($recordId);
     }
 }
