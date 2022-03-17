4424603f62684087098a5b37cc058c58045f161d
Mark Tompsett <mtompset@hotmail.com>
2022-03-17 16:21:23 -0400

    PSR-12 cleanup on tests

--- SilMock/tests/bootstrap.php
+++ SilMock/tests/bootstrap.php
@@ -1,25 +1,26 @@
 <?php
+
 //include_once('AutoLoader.php');
 // Register the directory to your include files
 //AutoLoader::registerDirectory(__DIR__ . '/../../SilMock');
 
-define('DATAFILE1', __DIR__.'/../DataStore/Sqlite/Test1_Google_Service_Data.db');
-define('DATAFILE2', __DIR__.'/../DataStore/Sqlite/Test2_Google_Service_Data.db');
-define('DATAFILE3', __DIR__.'/../DataStore/Sqlite/Test3_Google_Service_Data.db');
-define('DATAFILE4', __DIR__.'/../DataStore/Sqlite/Test4_Google_Service_Data.db');
+const DATAFILE1 = __DIR__ . '/../DataStore/Sqlite/Test1_Google_Service_Data.db';
+const DATAFILE2 = __DIR__ . '/../DataStore/Sqlite/Test2_Google_Service_Data.db';
+const DATAFILE3 = __DIR__ . '/../DataStore/Sqlite/Test3_Google_Service_Data.db';
+const DATAFILE4 = __DIR__ . '/../DataStore/Sqlite/Test4_Google_Service_Data.db';
 
-if(!file_exists(DATAFILE1)){
+if (!file_exists(DATAFILE1)) {
     touch(DATAFILE1);
 }
 
-if(!file_exists(DATAFILE2)){
+if (!file_exists(DATAFILE2)) {
     touch(DATAFILE2);
 }
 
-if(!file_exists(DATAFILE3)){
+if (!file_exists(DATAFILE3)) {
     touch(DATAFILE3);
 }
 
-if(!file_exists(DATAFILE4)){
+if (!file_exists(DATAFILE4)) {
     touch(DATAFILE4);
 }
