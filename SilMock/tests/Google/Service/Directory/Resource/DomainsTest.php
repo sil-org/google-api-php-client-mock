87042f621af2b8bc687bae1715cd60c79044d61c
Mark Tompsett <mtompset@hotmail.com>
2024-12-12 11:38:11 -0500

    Add more comments for clarity

--- SilMock/tests/Google/Service/Directory/Resource/DomainsTest.php
+++ SilMock/tests/Google/Service/Directory/Resource/DomainsTest.php
@@ -16,6 +16,9 @@ class DomainsTest extends TestCase
         foreach ($arrayOfDomainsObjects as $domainObject) {
             $domainNameList[$domainObject->getDomainName()] = $domainObject->getDomainName();
         }
+        // This should match the values in docker-compose.yml when testing locally
+        // This should match the values in run-tests.sh when testing is triggered by GitHub actions
+        // This is the keyed array version of the list.
         $expected = [
             'groups.example.org' => 'groups.example.org',
             'example.org' => 'example.org',
